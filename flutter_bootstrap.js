5fd6beaed5697b2f88339fbda034868b2d4b58bf
Ridho Octanio <40420988+dhotsky@users.noreply.github.com>
2026-04-06 10:36:06 +0700

    feat: update layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1445887943"
+    serviceWorkerVersion: "827250865"
   }
 });
